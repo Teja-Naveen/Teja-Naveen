294e3809656eba36ed67603941245b0f1d75bb5f
Teja-Naveen <161277351+Teja-Naveen@users.noreply.github.com>
2024-04-02 19:06:55 +0530

    Update CounterScript.js

--- js/CounterScript.js
+++ js/CounterScript.js
@@ -1,4 +1,24 @@
 // alert("🎁🎉🎂🎁 Advance Happy Birthday🎁🎉🎂🎁");
+/* Darkmode.js Settings */
+
+const options = {
+    bottom: "32px", // default: "32px"
+    right: "32px", // default: "32px"
+    left: "unset", // default: "unset"
+    time: "0.3s", // default: "0.3s"
+    mixColor: "#fff", // default: "#fff"
+    backgroundColor: "#fff",  // default: "#fff"
+    buttonColorDark: "#100f2c",  // default: "#100f2c"
+    buttonColorLight: "#fff", // default: "#fff"
+    saveInCookies: true, // default: true,
+    label: "<i class='fas fa-lg fa-moon text-white'></i>", // default: ""
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+const darkmodeButton = document.body.getElementsByTagName("button")[0];
 const dateSettingsButton = document.getElementById("date-settings");
 
 /* Date settings (UTC) */
@@ -54,9 +74,27 @@ const count = setInterval(() => {
 
 updateDateInformation();
 
-
+/** Changes the icon of the dark mode button */
+const darkModeButtonChangeIcon = (element, classesToToggle) => {
+    if (element == null || classesToToggle == null) {
+        return;
+    }
     classesToToggle.forEach(classToToggle => {
         element.classList.toggle(classToToggle);
     });
 };
 
+
+const darkmodeButtonClassList = ["text-warning", "text-white", "fa-moon", "fa-sun"];
+
+/** Change icon if dark mode is already active */
+if (document.body.classList.contains("darkmode--activated")) {
+    darkModeButtonChangeIcon(darkmodeButton.children[0], darkmodeButtonClassList);
+    darkModeButtonChangeIcon(dateSettingsButton, ["text-white"]);
+}
+
+/* Event listener for dark mode button */
+darkmodeButton.addEventListener("click", () => {
+    darkModeButtonChangeIcon(darkmodeButton.children[0], darkmodeButtonClassList);
+    darkModeButtonChangeIcon(dateSettingsButton, ["text-white"]);
+});
